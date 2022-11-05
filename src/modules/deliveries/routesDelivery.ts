8231c216bb4ef41d3eaa9662708c332fa0b01879
guilherme_freire <guilherme.freire@lionx.com.br>
2022-11-05 15:56:05 -0300

    feat: add AuthenticateDeliveryman and UseCases

--- src/modules/deliveries/routesDelivery.ts
+++ src/modules/deliveries/routesDelivery.ts
@@ -1,15 +1,40 @@
 import { Router } from "express";
 import { CreateDeliveryController } from "./useCases/createDelivery/CreateDeliveryController";
 import { ensureAuthenticateClient } from "../../middlewares/ensureAuthenticateClient";
+import { FindAllAvailableController } from "./useCases/findAllAvailable/FindAllAvailableController";
+import { ensureAuthenticateDeliveryman } from "../../middlewares/ensureAuthenticateDeliveryman";
+import { UpdateDeliverymanController } from "./useCases/updateDeliveryman/UpdateDeliverymanController";
+import { UpdateEndDateController } from "./useCases/updateEndDate/UpdateEndDateController";
 
 const routesDelivery = Router();
 
 const createDeliveryController = new CreateDeliveryController();
+const findAllAvailableController = new FindAllAvailableController();
+const updateDeliverymanController = new UpdateDeliverymanController();
+const updateEndCaseController = new UpdateEndDateController();
 
 routesDelivery.post(
   "/delivery/",
   ensureAuthenticateClient,
   createDeliveryController.handle
+);
+
+routesDelivery.get(
+  "/delivery/available/",
+  ensureAuthenticateDeliveryman,
+  findAllAvailableController.handle
+);
+
+routesDelivery.put(
+  "/delivery/updateDeliveryman/:id",
+  ensureAuthenticateDeliveryman,
+  updateDeliverymanController.handle
+);
+
+routesDelivery.put(
+  "/delivery/updateEndDate/:id",
+  ensureAuthenticateDeliveryman,
+  updateEndCaseController.handle
 );
 
 export { routesDelivery };
